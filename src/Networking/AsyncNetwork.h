f21c3fd98e6637ff8db5fe730ea1bd04415f5089
R2Sam <samuzena@gmail.com>
2026-03-08 01:29:34 +0200

    Need to test

---
+++ src/Networking/AsyncNetwork.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "NetworkCore.h"
+#include "Networking/NetworkTypes.h"
+#include "Types.h"
+
+#include "ThreadSafeQueue/concurrentqueue.h"
+#include <thread>
+#include <vector>
+
+class AsyncNetwork
+{
+public:
+
+	AsyncNetwork();
+	~AsyncNetwork();
+
+	bool InitServer(const u16 port, const u32 maxPeers = 64, const u32 channels = 1);
+	bool InitClient(const u32 channels = 1);
+	void ShutDown();
+
+	void Start();
+	void Stop();
+
+	void Connect(const Address& address, const u32 data = 0);
+	void Disconnect(const PeerId peerId, const u32 data = 0);
+
+	void Send(const PeerId peerId, std::vector<u8>&& data, const ChannelId channel = 0, const bool reliable = true);
+	void Send(const PeerId peerId, const u8* data, const u32 size, const ChannelId channel = 0,
+	const bool reliable = true);
+
+	bool PopEvent(NetworkEvent& event);
+
+private:
+
+	enum class CommandType : u8
+	{
+		CONNECT,
+		DISCONNECT,
+		SEND
+	};
+
+	struct Command
+	{
+		CommandType type;
+		PeerId peerId = 0;
+		Address address;
+		u32 extraData = 0;
+		std::vector<u8> data;
+		ChannelId channel = 0;
+		bool reliable = true;
+	};
+
+	void Loop();
+
+	NetworkCore m_core;
+	std::thread m_thread;
+	moodycamel::ConcurrentQueue<Command> m_commandQueue;
+	moodycamel::ConcurrentQueue<NetworkEvent> m_eventQueue;
+	std::atomic<bool> m_running = false;
+};
